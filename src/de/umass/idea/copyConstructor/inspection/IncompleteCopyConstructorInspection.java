948c3ff3c2f26a7ec7e0368077558f16ce8a4b75
SilverYoCha <yohann.chastagnier@silverpeas.com>
2019-01-24 09:22:52 +0100

    Making the inspection able to deal also with complex field initializations, like conditional expression for example. Fixing some IDE warnings and common SonarQube feedback.

--- src/de/umass/idea/copyConstructor/inspection/IncompleteCopyConstructorInspection.java
+++ src/de/umass/idea/copyConstructor/inspection/IncompleteCopyConstructorInspection.java
@@ -4,22 +4,7 @@ import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.psi.JavaElementVisitor;
-import com.intellij.psi.JavaRecursiveElementVisitor;
-import com.intellij.psi.PsiAssignmentExpression;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiExpression;
-import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiParameter;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiReferenceExpression;
-import com.intellij.psi.PsiThisExpression;
-import com.intellij.psi.PsiVariable;
-
+import com.intellij.psi.*;
 import de.umass.idea.copyConstructor.ConstructorUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -61,7 +46,7 @@ public class IncompleteCopyConstructorInspection extends LocalInspectionTool {
 		}
 
 		private boolean constructorAssignsAllFields(final PsiMethod constructor, List<PsiField> allFields) {
-			final Set<PsiField> unassignedFields = new HashSet<PsiField>(allFields);
+			final Set<PsiField> unassignedFields = new HashSet<>(allFields);
 			final PsiParameter copyParameter = constructor.getParameterList().getParameters()[0];
 			constructor.accept(new JavaRecursiveElementVisitor() {
 				@Override
@@ -71,7 +56,7 @@ public class IncompleteCopyConstructorInspection extends LocalInspectionTool {
 					PsiReference assignee = left.getReference();
 					if (assignee != null) {
 						PsiElement leftReference = assignee.resolve();
-						if (leftReference != null && leftReference instanceof PsiField) {
+						if (leftReference instanceof PsiField) {
 							PsiField referencedField = (PsiField) leftReference;
 							if (isReferenceToFieldInInstance(left, referencedField, null)) {
 								if (isReferenceToFieldInInstance(right, referencedField, copyParameter)) {
@@ -91,10 +76,60 @@ public class IncompleteCopyConstructorInspection extends LocalInspectionTool {
 		}
 
 		private boolean isReferenceToFieldInInstance(@Nullable PsiExpression expression, @NotNull PsiField field, @Nullable PsiVariable instance) {
-			if (expression != null && expression instanceof PsiReferenceExpression) {
+			boolean isReference = false;
+			if (expression instanceof PsiReferenceExpression) {
 				PsiReferenceExpression referenceExpression = (PsiReferenceExpression) expression;
 				PsiExpression qualifierExpression = referenceExpression.getQualifierExpression();
-				return referenceExpression.isReferenceTo(field) && qualifierReferencesVariable(qualifierExpression, instance);
+				isReference = referenceExpression.isReferenceTo(field) && qualifierReferencesVariable(qualifierExpression, instance);
+				if (!isReference && qualifierExpression instanceof PsiReferenceExpression) {
+					isReference = isReferenceToFieldInInstance(qualifierExpression, field, instance);
+				}
+			} else if (expression instanceof PsiCallExpression) {
+				PsiCallExpression callExpression = (PsiCallExpression) expression;
+				isReference = isCallExpressionReferencingToFieldInstance(callExpression, field, instance);
+			} else if (expression instanceof PsiPolyadicExpression) {
+				PsiPolyadicExpression polyadicExpression = (PsiPolyadicExpression) expression;
+				isReference = isPolyadicExpressionReferencingToFieldInstance(polyadicExpression, field, instance);
+			} else if (expression instanceof PsiConditionalExpression) {
+				PsiConditionalExpression conditionalExpression = (PsiConditionalExpression) expression;
+				isReference = isConditionalExpressionReferencingToFieldInstance(conditionalExpression, field, instance);
+			} else if (expression instanceof PsiParenthesizedExpression) {
+				PsiParenthesizedExpression parenthesizedExpression = (PsiParenthesizedExpression) expression;
+				isReference = isReferenceToFieldInInstance(parenthesizedExpression.getExpression(), field, instance);
+			}
+			return isReference;
+		}
+
+		private boolean isConditionalExpressionReferencingToFieldInstance(
+				final PsiConditionalExpression conditionalExpression, @NotNull final PsiField field,
+				@Nullable final PsiVariable instance) {
+			return isReferenceToFieldInInstance(conditionalExpression.getCondition(), field, instance)
+					&& (isReferenceToFieldInInstance(conditionalExpression.getThenExpression(), field, instance)
+						 || isReferenceToFieldInInstance(conditionalExpression.getElseExpression(), field, instance));
+		}
+
+		private boolean isPolyadicExpressionReferencingToFieldInstance(
+				final PsiPolyadicExpression polyadicExpression, @NotNull final PsiField field,
+				@Nullable final PsiVariable instance) {
+			for(PsiExpression operandExpression : polyadicExpression.getOperands()) {
+				if (isReferenceToFieldInInstance(operandExpression, field, instance)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private boolean isCallExpressionReferencingToFieldInstance(final PsiCallExpression callExpression,
+				@NotNull final PsiField field, @Nullable final PsiVariable instance) {
+			if (callExpression instanceof PsiMethodCallExpression) {
+				PsiMethodCallExpression methodExpression = (PsiMethodCallExpression) callExpression;
+				return isReferenceToFieldInInstance(methodExpression.getMethodExpression(), field, instance);
+			} else if (callExpression.getArgumentList() != null) {
+				for(PsiExpression argumentExpression : callExpression.getArgumentList().getExpressions()) {
+					if (isReferenceToFieldInInstance(argumentExpression, field, instance)) {
+						return true;
+					}
+				}
 			}
 			return false;
 		}
@@ -105,7 +140,7 @@ public class IncompleteCopyConstructorInspection extends LocalInspectionTool {
 
 			if (qualifier != null) {
 				PsiReference reference = qualifier.getReference();
-				return reference != null && reference.isReferenceTo(instance);
+				return reference != null && instance != null && reference.isReferenceTo(instance);
 			}
 
 			return false;
